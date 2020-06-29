419948ad2f2454f5ad50d65077dd15ed81931526
GongYunhao <gongyunhao@sina.com>
2020-06-30 00:20:17 +0800

    增加Sum Root to Leaf Numbers代码与测试;

---
+++ src/Leetcode/SumRootToLeafNumbers_129.cs
@@ -0,0 +1,31 @@
+﻿using CommonModels;
+
+namespace Leetcode
+{
+    public class SumRootToLeafNumbers_129
+    {
+        public int SumNumbers(TreeNode root)
+        {
+            int total = 0;
+            SumNumbersRecursively(root, 0,ref total);
+            return total;
+        }
+
+        private void SumNumbersRecursively(TreeNode subTreeRoot,int baseValue,ref int total)
+        {
+            if (subTreeRoot == null)
+                return;
+
+            baseValue += subTreeRoot.val;
+
+            if (subTreeRoot.left == null && subTreeRoot.right == null)
+            {
+                total += baseValue;
+                return;
+            }
+
+            SumNumbersRecursively(subTreeRoot.left, baseValue * 10, ref total);
+            SumNumbersRecursively(subTreeRoot.right, baseValue * 10, ref total);
+        }
+    }
+}
---
+++ src/LeetcodeTest/SumRootToLeafNumbersTest.cs
@@ -0,0 +1,93 @@
+﻿using CommonModels;
+using Leetcode;
+using Xunit;
+
+namespace LeetcodeTest
+{
+    public class SumRootToLeafNumbersTest
+    {
+        [Fact]
+        public void TestWithNullRoot()
+        {
+            var target = new SumRootToLeafNumbers_129();
+            Assert.Equal(0, target.SumNumbers(null));
+        }
+
+        [Fact]
+        public void TestWithSingleNodeTree()
+        {
+            var target = new SumRootToLeafNumbers_129();
+
+            var tree = new TreeNode(9);
+
+            Assert.Equal(9, target.SumNumbers(tree));
+        }
+
+        [Fact]
+        public void TestWithOneLevelDepthChildren()
+        {
+            var target = new SumRootToLeafNumbers_129();
+
+            var tree1 = new TreeNode(9)
+            {
+                left = new TreeNode(5)
+            };
+
+            Assert.Equal(95, target.SumNumbers(tree1));
+
+            var tree2 = new TreeNode(9)
+            {
+                right = new TreeNode(5)
+            };
+
+            Assert.Equal(95, target.SumNumbers(tree2));
+
+            var tree3 = new TreeNode(9)
+            {
+                left = new TreeNode(5),
+                right = new TreeNode(5)
+            };
+
+            Assert.Equal(190, target.SumNumbers(tree3));
+        }
+
+        [Fact]
+        public void TestWithOnlyLeftChildren()
+        {
+            var target = new SumRootToLeafNumbers_129();
+
+            var tree1 = new TreeNode(9)
+            {
+                left = new TreeNode(5)
+                {
+                    left  = new TreeNode(5)
+                    {
+                        left = new TreeNode(5)
+                    }
+                }
+            };
+
+            Assert.Equal(9555, target.SumNumbers(tree1));
+        }
+
+        [Fact]
+        public void TestWithUnbalancedTree()
+        {
+            var target = new SumRootToLeafNumbers_129();
+
+            var tree1 = new TreeNode(9)
+            {
+                left = new TreeNode(5)
+                {
+                    left = new TreeNode(5)
+                    {
+                        left = new TreeNode(5)
+                    }
+                },
+                right= new TreeNode(1)
+            };
+
+            Assert.Equal(9646, target.SumNumbers(tree1));
+        }
+    }
+}
