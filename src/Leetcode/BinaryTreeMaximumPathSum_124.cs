6cd7cba188df0b0147ad55c6cb4cb04a8317fc2d
GongYunhao <gongyunhao@sina.com>
2020-07-03 17:34:10 +0800

    增加"124二叉树中的最大路径和"测试代码与部分题解;

--- src/Leetcode/BinaryTreeMaximumPathSum_124.cs
+++ src/Leetcode/BinaryTreeMaximumPathSum_124.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Diagnostics;
-using CommonModels;
+﻿using CommonModels;
 
 namespace Leetcode
 {
-    public class BinaryTreeMaximumPathSum_129
+    public class BinaryTreeMaximumPathSum_124
     {
-        private int result = int.MinValue;
+        /// <summary>
+        /// 在整个计算过程中,暂存最大的路径数值之和<br/>
+        /// 当计算完成时,此变量中保存的即为整个树结构对应的算法结果
+        /// </summary>
+        private int result;
 
         public int MaxPathSum(TreeNode root)
         {
+            result = root.val; // 根据题目,根节点不能为Null,所以可以使用根节点的值作为初始值
+
             MaxPathSumRecursively(root);
 
             return result;
@@ -17,37 +21,51 @@ namespace Leetcode
 
         private int MaxPathSumRecursively(TreeNode subTreeRoot)
         {
+            // 分别计算左子树和右子树对应的最大数值
+            // 若不包含左右子树,则变量赋值为Null
             int? left = subTreeRoot.left == null ? null : (int?)MaxPathSumRecursively(subTreeRoot.left);
             int? right = subTreeRoot.right == null ? null : (int?)MaxPathSumRecursively(subTreeRoot.right);
 
+            // 比对左右子树返回结果,将最大值存入result变量
             ReplaceResultWithLarger(left);
             ReplaceResultWithLarger(right);
 
+            // 如果根节点同时拥有左右子树,那么存在一种可能的路径
+            // 即路径从左子树出发,经过根节点,通向右子树内部
+            // 这种情况需要计算其路径的最大值,并与result变量比对
+            // 但是这种路径不可能继续延伸到根节点的父节点,所以无函数返回值
             if(HasAboveZeroValue(left) && HasAboveZeroValue(right))
             {
                 ReplaceResultWithLarger(subTreeRoot.val + left.Value + right.Value);
             }
 
+            // 接下来计算路径可能通向父节点的情况
+            // 即 根节点自身 或 左子树->根节点 或 右子树->根节点(左右子树的返回值必须大于0才能参与比对)
+            // 这几种情况不仅需要与result变量比对,还需要返回值,让父节点做进一步处理
             if (HasAboveZeroValue(left) && HasAboveZeroValue(right))
             {
+                // 如果根节点同时拥有左右子树,那么我们选取其中最大的一条,加上根节点作为可能的路径
                 var pathVal = subTreeRoot.val + (left.Value > right.Value ? left.Value : right.Value);
                 ReplaceResultWithLarger(pathVal);
                 return pathVal;
             }
             else if (HasAboveZeroValue(right))
             {
+                // 如果根节点仅拥有右子树
                 var pathVal = subTreeRoot.val + right.Value;
                 ReplaceResultWithLarger(pathVal);
                 return pathVal;
             }
             else if (HasAboveZeroValue(left))
             {
+                // 如果根节点仅拥有左子树
                 var pathVal = subTreeRoot.val + left.Value;
                 ReplaceResultWithLarger(pathVal);
                 return pathVal;
             }
             else
             {
+                // 仅根节点自身返回给父节点
                 var pathVal = subTreeRoot.val;
                 ReplaceResultWithLarger(pathVal);
                 return pathVal;
@@ -63,6 +81,10 @@ namespace Leetcode
                 result = newVal.Value;
         }
 
+        /// <summary>
+        /// 判断是否有子树<br/>
+        /// 并且子树的返回值必须大于0才有进一步判断的意义
+        /// </summary>
         private bool HasAboveZeroValue(int? val)
         {
             return val.HasValue && val.Value > 0;
---
+++ src/LeetcodeTest/BinaryTreeMaximumPathSumTest.cs
@@ -0,0 +1,92 @@
+﻿using CommonModels;
+using Leetcode;
+using Xunit;
+
+namespace LeetcodeTest
+{
+    public class BinaryTreeMaximumPathSumTest
+    {
+        [Fact]
+        public void TestWithSingleNodeTree()
+        {
+            Assert.Equal(3, new BinaryTreeMaximumPathSum_124().MaxPathSum(new TreeNode(3)));
+            Assert.Equal(0, new BinaryTreeMaximumPathSum_124().MaxPathSum(new TreeNode(0)));
+            Assert.Equal(-1, new BinaryTreeMaximumPathSum_124().MaxPathSum(new TreeNode(-1)));
+
+            Assert.Equal(int.MinValue,
+                new BinaryTreeMaximumPathSum_124().MaxPathSum(new TreeNode(int.MinValue)));
+            Assert.Equal(int.MaxValue,
+                new BinaryTreeMaximumPathSum_124().MaxPathSum(new TreeNode(int.MaxValue)));
+        }
+
+        [Fact]
+        public void TestWithNormalTree_TargetPathLimitedInLeftNode()
+        {
+            var target = new BinaryTreeMaximumPathSum_124();
+
+            var tree = new TreeNode(-3)
+            {
+                left = new TreeNode(-1) // 路径限定在左子节点内部
+                {
+                    left = new TreeNode(20),
+                    right = new TreeNode(-2)
+                }
+            };
+
+            Assert.Equal(20, target.MaxPathSum(tree));
+        }
+
+        [Fact]
+        public void TestWithNormalTree_TargetPathIncludeRootNode()
+        {
+            var target = new BinaryTreeMaximumPathSum_124();
+
+            var tree = new TreeNode(-3)
+            {
+                left = new TreeNode(5) // 路径包括左子节点和根节点
+                {
+                    left = new TreeNode(20),
+                    right = new TreeNode(-2)
+                }
+            };
+
+            Assert.Equal(25, target.MaxPathSum(tree));
+        }
+
+        [Fact]
+        public void TestWithNormalTree_TargetPathIncludeRootNodeAndParent()
+        {
+            var target = new BinaryTreeMaximumPathSum_124();
+
+            var tree = new TreeNode(5)
+            {
+                left = new TreeNode(5) // 路径包括左子节点,根节点以及父节点
+                {
+                    left = new TreeNode(20),
+                    right = new TreeNode(-2)
+                },
+                right = new TreeNode(-2)
+            };
+
+            Assert.Equal(30, target.MaxPathSum(tree));
+        }
+
+        [Fact]
+        public void TestWithNormalTree_TargetPathIncludeLeftRootAndRight()
+        {
+            var target = new BinaryTreeMaximumPathSum_124();
+
+            var tree = new TreeNode(5)
+            {
+                left = new TreeNode(5) // 路径包括左子节点,根节点以及右子节点
+                {
+                    left = new TreeNode(20),
+                    right = new TreeNode(6)
+                },
+                right = new TreeNode(-2)
+            };
+
+            Assert.Equal(31, target.MaxPathSum(tree));
+        }
+    }
+}
