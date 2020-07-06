c30405b94dfdcfc1a556d3ea417b55e2dd67ed27
GongYunhao <gongyunhao@sina.com>
2020-07-06 22:36:44 +0800

    增加'100判断二叉树是否相同'代码与解析;

---
+++ src/Leetcode/SameTree_100.cs
@@ -0,0 +1,18 @@
+﻿using CommonModels;
+
+namespace Leetcode
+{
+    public class SameTree_100
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null)
+                return q == null;
+
+            return q != null
+                && p.val == q.val
+                && IsSameTree(p.left, q.left)
+                && IsSameTree(p.right, q.right);
+        }
+    }
+}
---
+++ src/LeetcodeTest/SameTreeTest.cs
@@ -0,0 +1,70 @@
+﻿using CommonModels;
+using Leetcode;
+using Xunit;
+
+namespace LeetcodeTest
+{
+    public class SameTreeTest
+    {
+        [Fact]
+        public void TestWithNullRootTree()
+        {
+            var target = new SameTree_100();
+            Assert.True(target.IsSameTree(null, null));
+
+            var q = new TreeNode(5);
+            Assert.False(target.IsSameTree(q, null));
+            Assert.False(target.IsSameTree(null, q));
+        }
+
+        [Fact]
+        public void TestWithSingleNodeTree()
+        {
+            var target = new SameTree_100();
+            Assert.True(target.IsSameTree(new TreeNode(5), new TreeNode(5)));
+            Assert.False(target.IsSameTree(new TreeNode(5), new TreeNode(10)));
+        }
+
+        [Fact]
+        public void TestWithNormalTree_SameShape()
+        {
+            var target = new SameTree_100();
+            var p = new TreeNode(5)
+            {
+                left = new TreeNode(3),
+                right = new TreeNode(7)
+            };
+            var q = new TreeNode(5)
+            {
+                left = new TreeNode(3),
+                right = new TreeNode(7)
+            };
+            var q1 = new TreeNode(5)
+            {
+                left = new TreeNode(7),
+                right = new TreeNode(3)
+            };
+
+
+            Assert.True(target.IsSameTree(p, q));
+            Assert.False(target.IsSameTree(p, q1));
+        }
+
+        [Fact]
+        public void TestWithNormalTree_DiffShape()
+        {
+            var target = new SameTree_100();
+            var p = new TreeNode(5)
+            {
+                left = new TreeNode(3),
+                right = new TreeNode(7)
+            };
+            var q = new TreeNode(5)
+            {
+                left = new TreeNode(3)
+            };
+
+            Assert.False(target.IsSameTree(p, q));
+        }
+    }
+}
