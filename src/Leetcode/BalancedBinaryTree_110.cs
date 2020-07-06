f7f42f31869a5e8b53f8500ed44c22b7ddac6513
GongYunhao <gongyunhao@sina.com>
2020-07-07 00:02:28 +0800

    增加'110平衡二叉树'代码与解析;

---
+++ src/Leetcode/BalancedBinaryTree_110.cs
@@ -0,0 +1,47 @@
+﻿using CommonModels;
+
+namespace Leetcode
+{
+    public class BalancedBinaryTree_110
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            bool result;
+            (result, _) = GetSubTreeHeight(root);
+            return result;
+        }
+
+        private (bool, int) GetSubTreeHeight(TreeNode subTreeRoot)
+        {
+            if (subTreeRoot == null)
+                return (true, 0);
+
+            int leftHeight = 0, rightHeight = 0;
+            if (subTreeRoot.left != null)
+            {
+                bool leftBalanced;
+                (leftBalanced, leftHeight) = GetSubTreeHeight(subTreeRoot.left);
+                if (!leftBalanced)
+                {
+                    return (false, 0);// if left subtree is unbalanced, whole tree is unbalanced, so we can take a shortcut here, no need to visit right subtree
+                }
+            }
+            if (subTreeRoot.right != null)
+            {
+                bool rightBalanced;
+                (rightBalanced, rightHeight) = GetSubTreeHeight(subTreeRoot.right);
+                if (!rightBalanced)
+                {
+                    return (false, 0);
+                }
+            }
+
+            // both left and right subtrees are balanced
+            // check if root node balanced
+            int bigger = leftHeight > rightHeight ? leftHeight : rightHeight;
+            int smaller = leftHeight > rightHeight ? rightHeight : leftHeight;
+
+            return (bigger - smaller <= 1, bigger + 1);
+        }
+    }
+}
---
+++ src/LeetcodeTest/BalancedBinaryTreeTest.cs
@@ -0,0 +1,102 @@
+﻿using CommonModels;
+using Leetcode;
+using Xunit;
+
+namespace LeetcodeTest
+{
+    public class BalancedBinaryTreeTest
+    {
+        [Fact]
+        public void TestWithNullRoot()
+        {
+            Assert.True(new BalancedBinaryTree_110().IsBalanced(null));
+        }
+
+        [Fact]
+        public void TestWithSingleNodeRoot()
+        {
+            var target = new BalancedBinaryTree_110();
+            var tree = new TreeNode(30);
+            Assert.True(target.IsBalanced(tree));
+        }
+
+        [Fact]
+        public void TestWithTwoLevelTree_AlwaysBalanced()
+        {
+            var target = new BalancedBinaryTree_110();
+            var tree = new TreeNode(30)
+            {
+                left = new TreeNode(10)
+            };
+            Assert.True(target.IsBalanced(tree));
+        }
+
+        [Fact]
+        public void TestWithThreeLevelTree_Balanced()
+        {
+            var target = new BalancedBinaryTree_110();
+            var tree = new TreeNode(30)
+            {
+                left = new TreeNode(10)
+                {
+                    left = new TreeNode(1)
+                },
+                right = new TreeNode(45)
+            };
+            Assert.True(target.IsBalanced(tree));
+        }
+
+        [Fact]
+        public void TestWithThreeLevelTree_Unbalanced()
+        {
+            var target = new BalancedBinaryTree_110();
+            var tree = new TreeNode(30)
+            {
+                left = new TreeNode(10)
+                {
+                    left = new TreeNode(1)
+                }
+            };
+            Assert.False(target.IsBalanced(tree));
+        }
+
+        [Fact]
+        public void TestWithTree_Balanced()
+        {
+            var target = new BalancedBinaryTree_110();
+            var tree = new TreeNode(30)
+            {
+                left = new TreeNode(10)
+                {
+                    left = new TreeNode(1)
+                    {
+                        right = new TreeNode(6)
+                    },
+                    right = new TreeNode(20)
+                },
+                right = new TreeNode(45)
+                {
+                    left = new TreeNode(33)
+                }
+            };
+            Assert.True(target.IsBalanced(tree));
+        }
+
+        [Fact]
+        public void TestWithTree_Unalanced()
+        {
+            var target = new BalancedBinaryTree_110();
+            var tree = new TreeNode(30)
+            {
+                left = new TreeNode(10)
+                {
+                    left = new TreeNode(1)
+                    {
+                        left = new TreeNode(6)
+                    }
+                }
+            };
+            Assert.False(target.IsBalanced(tree));
+        }
+    }
+}
