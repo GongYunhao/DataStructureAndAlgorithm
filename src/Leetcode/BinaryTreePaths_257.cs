59a6762a3bd0162cbb698da744a9d075eb3daa81
GongYunhao <gongyunhao@sina.com>
2020-07-21 00:02:32 +0800

    增加"二叉树的所有路径"代码,测试用例与解析;

---
+++ src/Leetcode/BinaryTreePaths_257.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using CommonModels;
+
+namespace Leetcode
+{
+    public class BinaryTreePaths_257
+    {
+        public IList<string> BinaryTreePaths(TreeNode root)
+        {
+            var result = new List<string>();
+
+            if (root == null)
+                return result;
+
+            BinaryTreePathRecursion(root, string.Empty, result);
+            return result;
+        }
+
+        private void BinaryTreePathRecursion(TreeNode subTreeRoot, string currentStr, IList<string> result)
+        {
+            currentStr += subTreeRoot.val;
+
+            if (subTreeRoot.left == null && subTreeRoot.right == null)
+            {
+                result.Add(currentStr);
+                return;
+            }
+
+            currentStr += "->";
+
+            if (subTreeRoot.left != null)
+            {
+                BinaryTreePathRecursion(subTreeRoot.left, currentStr, result);
+            }
+            if (subTreeRoot.right != null)
+            {
+                BinaryTreePathRecursion(subTreeRoot.right, currentStr, result);
+            }
+        }
+    }
+}
---
+++ src/LeetcodeTest/BinaryTreePathsTest.cs
@@ -0,0 +1,48 @@
+﻿using CommonModels;
+using Leetcode;
+using Xunit;
+
+namespace LeetcodeTest
+{
+    public class BinaryTreePathsTest
+    {
+        [Fact]
+        public void TestWithNullRoot()
+        {
+            Assert.Empty(new BinaryTreePaths_257().BinaryTreePaths(null));
+        }
+
+        [Fact]
+        public void TestWithSingleNodeTree()
+        {
+            var algo = new BinaryTreePaths_257();
+
+            var root = new TreeNode(10);
+
+            var result = algo.BinaryTreePaths(root);
+
+            Assert.Single(result);
+            Assert.Equal("10", result[0]);
+        }
+
+        [Fact]
+        public void TestWithNormalTree()
+        {
+            var algo = new BinaryTreePaths_257();
+
+            var root = new TreeNode(10)
+            {
+                left = new TreeNode(2)
+                {
+                    right = new TreeNode(5)
+                },
+                right = new TreeNode(3)
+            };
+
+            var result = algo.BinaryTreePaths(root);
+
+            Assert.Equal(2, result.Count);
+            Assert.True(result.ContainsSameElement(new[] { "10->2->5", "10->3" }));
+        }
+    }
+}
