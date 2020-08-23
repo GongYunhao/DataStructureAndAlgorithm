615546247ca61ff2eacd3a36c733bab4cc6deb43
GongYunhao <gongyunhao@sina.com>
2020-08-23 23:54:34 +0800

    增加"222.计算完全二叉树节点数量"代码;

---
+++ src/Leetcode/CountCompleteTreeNodes_222.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using CommonModels;
+
+namespace Leetcode
+{
+    public class CountCompleteTreeNodes_222
+    {
+        public int CountNodes(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int maxHeight = GetMaxHeight(root);
+
+            int left = 0;
+            int right = (int)Math.Pow(2, maxHeight - 1) - 1;
+            int mask = (right + 1) >> 1;
+
+            if (HasLeafOnRoute(root, right, mask))
+            {
+                return (int)Math.Pow(2, maxHeight) - 1;
+            }
+
+            while (left < right - 1)
+            {
+                int middle = (left + right) >> 1;
+                if (HasLeafOnRoute(root, middle, mask))
+                {
+                    left = middle;
+                }
+                else
+                {
+                    right = middle;
+                }
+            }
+
+            return (int)Math.Pow(2, maxHeight - 1) - 1 + right;
+        }
+
+        private bool HasLeafOnRoute(TreeNode root, int routeInt, int mask)
+        {
+            var currNode = root;
+            while (mask > 0)
+            {
+                if ((routeInt & mask) == 0)
+                {
+                    currNode = currNode.left;
+                }
+                else
+                {
+                    currNode = currNode.right;
+                }
+
+                mask >>= 1;
+            }
+
+            return currNode != null;
+        }
+
+        private int GetMaxHeight(TreeNode root)
+        {
+            int height = 0;
+            while (root != null)
+            {
+                height++;
+                root = root.left;
+            }
+            return height;
+        }
+    }
+}
---
+++ src/LeetcodeTest/CountCompleteTreeNodesTest.cs
@@ -0,0 +1,79 @@
+﻿using CommonModels;
+using Leetcode;
+using Xunit;
+
+namespace LeetcodeTest
+{
+    public class CountCompleteTreeNodesTest
+    {
+        [Fact]
+        public void TestWithNullRoot()
+        {
+            Assert.Equal(0, new CountCompleteTreeNodes_222().CountNodes(null));
+        }
+
+        [Fact]
+        public void TestWithSingleRoot()
+        {
+            Assert.Equal(1, new CountCompleteTreeNodes_222().CountNodes(new TreeNode(1)));
+        }
+
+        [Fact]
+        public void TestWithIncompleteTree_FinalLeafIsLeft()
+        {
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(4),
+                    right = new TreeNode(5)
+                },
+                right = new TreeNode(3)
+                {
+                    left = new TreeNode(6)
+                }
+            };
+
+            Assert.Equal(6, new CountCompleteTreeNodes_222().CountNodes(root));
+        }
+
+        [Fact]
+        public void TestWithIncompleteTree_FinalLeafIsRight()
+        {
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(4),
+                    right = new TreeNode(5)
+                },
+                right = new TreeNode(3)
+                {
+
+                }
+            };
+
+            Assert.Equal(5, new CountCompleteTreeNodes_222().CountNodes(root));
+        }
+
+        [Fact]
+        public void TestWithCompleteTree()
+        {
+            var root = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(4),
+                    right = new TreeNode(5)
+                },
+                right = new TreeNode(3)
+                {
+                    left = new TreeNode(6),
+                    right = new TreeNode(7)
+                }
+            };
+
+            Assert.Equal(7, new CountCompleteTreeNodes_222().CountNodes(root));
+        }
+    }
+}
