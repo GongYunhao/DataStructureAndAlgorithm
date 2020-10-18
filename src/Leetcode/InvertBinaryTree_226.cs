ebea5fa15c712b50bbc6352ad23a6421dfcdd966
GongYunhao <gongyunhao@sina.com>
2020-10-18 22:40:51 +0800

    增加两个算法代码与解析;

---
+++ src/Leetcode/InvertBinaryTree_226.cs
@@ -0,0 +1,21 @@
+﻿using CommonModels;
+
+namespace Leetcode
+{
+    public class InvertBinaryTree_226
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root != null)
+            {
+                var left = InvertTree(root.left);
+                var right = InvertTree(root.right);
+
+                root.left = right;
+                root.right = left;
+            }
+
+            return root;
+        }
+    }
+}
---
+++ src/Leetcode/LowestCommonAncestorofaBinarySearchTree_235.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonModels;
+
+namespace Leetcode
+{
+    public class LowestCommonAncestorofaBinarySearchTree_235
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            int pVal = p.val, qVal = q.val;
+
+            TreeNode currNode = root;
+
+            while (currNode != null)
+            {
+                if (currNode.val > pVal && currNode.val > qVal)
+                {
+                    currNode = currNode.left;
+                }
+                else if (currNode.val < pVal && currNode.val < qVal)
+                {
+                    currNode = currNode.right;
+                }
+                else
+                {
+                    return currNode;
+                }
+            }
+
+            return null;
+        }
+    }
+}
