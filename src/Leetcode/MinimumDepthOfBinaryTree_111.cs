982d01954a893328a401b22cc2ff6d2925403b72
GongYunhao <gongyunhao@sina.com>
2020-12-16 22:55:27 +0800

    增加算法111;

---
+++ src/Leetcode/MinimumDepthOfBinaryTree_111.cs
@@ -0,0 +1,31 @@
+﻿using CommonModels;
+using System;
+
+namespace Leetcode
+{
+    public class MinimumDepthOfBinaryTree_111
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            if (root.left == null && root.right == null)
+            {
+                return 1;
+            }
+            else if (root.left == null && root.right != null)
+            {
+                return MinDepth(root.right) + 1;
+            }
+            else if (root.left != null && root.right == null)
+            {
+                return MinDepth(root.left) + 1;
+            }
+            else
+            {
+                return Math.Min(MinDepth(root.left), MinDepth(root.right)) + 1;
+            }
+        }
+    }
+}
