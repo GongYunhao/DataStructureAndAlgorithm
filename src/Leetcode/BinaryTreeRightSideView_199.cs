bb2feedb5b67f48b5ab5144ceda166a69950e44b
GongYunhao <gongyunhao@sina.com>
2020-12-27 23:16:55 +0800

    增加算法199;

---
+++ src/Leetcode/BinaryTreeRightSideView_199.cs
@@ -0,0 +1,29 @@
+﻿using CommonModels;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    public class BinaryTreeRightSideView_199
+    {
+        public IList<int> RightSideView(TreeNode root)
+        {
+            IList<int> result = new List<int>();
+            RightSideView(root, 0, ref result);
+            return result;
+        }
+
+        private void RightSideView(TreeNode subTreeRoot, int currentLevel, ref IList<int> result)
+        {
+            if (subTreeRoot == null)
+                return;
+
+            if (result.Count < currentLevel + 1)
+            {
+                result.Add(subTreeRoot.val);
+            }
+
+            RightSideView(subTreeRoot.right, currentLevel + 1, ref result);
+            RightSideView(subTreeRoot.left, currentLevel + 1, ref result);
+        }
+    }
+}
---
+++ src/LeetcodeTest/BinaryTreeRightSideViewTest.cs
@@ -0,0 +1,133 @@
+﻿using CommonModels;
+using Leetcode;
+using Xunit;
+
+namespace LeetcodeTest
+{
+    public class BinaryTreeRightSideViewTest
+    {
+        [Fact]
+        public void TestNullRoot()
+        {
+            var algo = new BinaryTreeRightSideView_199();
+            var list = algo.RightSideView(null);
+            Assert.Empty(list);
+        }
+
+        [Fact]
+        public void TestWithOneNodeTree()
+        {
+            var algo = new BinaryTreeRightSideView_199();
+            var list = algo.RightSideView(new TreeNode(20));
+            Assert.Single(list);
+            Assert.Equal(20, list[0]);
+        }
+
+        [Fact]
+        public void TestWithCommonTree1()
+        {
+            var algo = new BinaryTreeRightSideView_199();
+
+            var tree = new TreeNode(3)
+            {
+                right = new TreeNode(5),
+                left = new TreeNode(20)
+            };
+
+            var list = algo.RightSideView(tree);
+            Assert.Equal(2, list.Count);
+            Assert.Equal(3, list[0]);
+            Assert.Equal(5, list[1]);
+        }
+
+        [Fact]
+        public void TestWithCommonTree2()
+        {
+            var algo = new BinaryTreeRightSideView_199();
+
+            var tree = new TreeNode(3)
+            {
+                right = new TreeNode(5)
+                {
+                    left = new TreeNode(23)
+                },
+                left = new TreeNode(20)
+            };
+
+            var list = algo.RightSideView(tree);
+            Assert.Equal(3, list.Count);
+            Assert.Equal(3, list[0]);
+            Assert.Equal(5, list[1]);
+            Assert.Equal(23, list[2]);
+        }
+
+        [Fact]
+        public void TestWithCommonTree3()
+        {
+            var algo = new BinaryTreeRightSideView_199();
+
+            var tree = new TreeNode(3)
+            {
+                right = new TreeNode(5),
+                left = new TreeNode(20)
+                {
+                    left = new TreeNode(23)
+                }
+            };
+
+            var list = algo.RightSideView(tree);
+            Assert.Equal(3, list.Count);
+            Assert.Equal(3, list[0]);
+            Assert.Equal(5, list[1]);
+            Assert.Equal(23, list[2]);
+        }
+
+        [Fact]
+        public void TestWithOneSide1()
+        {
+            var algo = new BinaryTreeRightSideView_199();
+
+            var tree = new TreeNode(3)
+            {
+                left = new TreeNode(20)
+                {
+                    left = new TreeNode(23)
+                    {
+                        left = new TreeNode(88)
+                    }
+                }
+            };
+
+            var list = algo.RightSideView(tree);
+            Assert.Equal(4, list.Count);
+            Assert.Equal(3, list[0]);
+            Assert.Equal(20, list[1]);
+            Assert.Equal(23, list[2]);
+            Assert.Equal(88, list[3]);
+        }
+
+        [Fact]
+        public void TestWithOneSide2()
+        {
+            var algo = new BinaryTreeRightSideView_199();
+
+            var tree = new TreeNode(3)
+            {
+                right = new TreeNode(20)
+                {
+                    right = new TreeNode(23)
+                    {
+                        right = new TreeNode(88)
+                    }
+                }
+            };
+
+            var list = algo.RightSideView(tree);
+            Assert.Equal(4, list.Count);
+            Assert.Equal(3, list[0]);
+            Assert.Equal(20, list[1]);
+            Assert.Equal(23, list[2]);
+            Assert.Equal(88, list[3]);
+        }
+    }
+}
